e1b7f6d5dbd880348d02c15676c96d24fb05d6e1
sonjh919 <kingjh1125@gmail.com>
2025-03-02 14:03:04 +0900

    feat: 람다로 sum 중복 제거

--- java-language/initial/src/test/java/cholog/FunctionalProgrammingTest.java
+++ java-language/initial/src/test/java/cholog/FunctionalProgrammingTest.java
@@ -244,44 +244,27 @@ public class FunctionalProgrammingTest {
             class Calculator {
                 // TODO: 람다를 활용하여 sum 메서드를 통해 중복을 제거하세요.
                 static int sumAll(final List<Integer> numbers) {
-                    var total = 0;
-                    for (final var number : numbers) {
-                        total += number;
-                    }
-
-                    return total;
+                    return sum(numbers, number -> true);
                 }
 
                 // TODO: 람다를 활용하여 sum 메서드를 통해 중복을 제거하세요.
                 static int sumAllEven(final List<Integer> numbers) {
-                    var total = 0;
-                    for (final var number : numbers) {
-                        if (number % 2 == 0) {
-                            total += number;
-                        }
-                    }
-
-                    return total;
+                    return sum(numbers, number -> number % 2 == 0);
                 }
 
                 // TODO: 람다를 활용하여 sum 메서드를 통해 중복을 제거하세요.
                 static int sumAllOverThree(final List<Integer> numbers) {
-                    var total = 0;
-                    for (final var number : numbers) {
-                        if (number > 3) {
-                            total += number;
-                        }
-                    }
-
-                    return total;
+                    return sum(numbers, number -> number > 3);
                 }
 
                 private static int sum(
                         final List<Integer> numbers,
                         final Predicate<Integer> condition
                 ) {
-                    // TODO: 조건에 맞게 필터링하여 합계를 구하는 기능을 구현하세요.
-                    return 0;
+                    return numbers.stream()
+                            .filter(condition)
+                            .reduce(Integer::sum)
+                            .orElse(0);
                 }
             }
 
